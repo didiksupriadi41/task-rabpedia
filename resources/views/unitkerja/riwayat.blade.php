e5f203f3047f2ec32f8549f224efd996d59c5d58
I Putu Gede Wirasuta <wirasutat@gmail.com>
2020-02-20 20:54:18 +0700

    implement history accordion header with static data

--- resources/views/unitkerja/riwayat.blade.php
+++ resources/views/unitkerja/riwayat.blade.php
@@ -6,15 +6,18 @@
 <div class="mt-4">
     <div class="accordion" id="accordionHistory">
         <div class="card">
-            <div class="card-header" id="headingOne">
+            <div class="card-header d-flex justify-content-between" id="headingOne">
                 <h2 class="mb-0">
-                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne"
-                        aria-expanded="true" aria-controls="collapseOne">
-                        Collapsible Group Item #1
+                    <button class="btn btn-link text-left text-dark" type="button" data-toggle="collapse"
+                        data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        <span>Pengajuan Perbaikan Lorong Lt4 Labtek V</span><br>
+                        <span>Jumat, 14 Februari 2020</span>
                     </button>
                 </h2>
+                <div class="d-flex align-items-center">
+                    <span class="status-pill bg-warning">Diproses Ditlog</span>
+                </div>
             </div>
-
             <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionHistory">
                 <div class="card-body">
                     Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
@@ -31,13 +34,17 @@
             </div>
         </div>
         <div class="card">
-            <div class="card-header" id="headingTwo">
+            <div class="card-header d-flex justify-content-between" id="headingTwo">
                 <h2 class="mb-0">
-                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                    <button class="btn btn-link text-left text-dark collapsed" type="button" data-toggle="collapse"
                         data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        Collapsible Group Item #2
+                        <span>Pengajuan Perbaikan Kelas 7602</span><br>
+                        <span>Jumat, 7 Februari 2020</span>
                     </button>
                 </h2>
+                <div class="d-flex align-items-center">
+                    <span class="status-pill bg-warning">Dikerjakan</span>
+                </div>
             </div>
             <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionHistory">
                 <div class="card-body">
@@ -55,15 +62,47 @@
             </div>
         </div>
         <div class="card">
-            <div class="card-header" id="headingThree">
+            <div class="card-header d-flex justify-content-between" id="headingThree">
                 <h2 class="mb-0">
-                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                    <button class="btn btn-link text-left text-dark collapsed" type="button" data-toggle="collapse"
                         data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        Collapsible Group Item #3
+                        <span>Pengajuan Perbaikan Kelas 7601</span><br>
+                        <span>Jumat, 5 Februari 2020</span>
                     </button>
                 </h2>
+                <div class="d-flex align-items-center">
+                    <span class="status-pill bg-success text-white">Selesai</span>
+                </div>
             </div>
             <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionHistory">
+                <div class="card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
+                    wolf
+                    moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
+                    eiusmod.
+                    Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
+                    shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente
+                    ea
+                    proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw
+                    denim
+                    aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header d-flex justify-content-between" id="headingFour">
+                <h2 class="mb-0">
+                    <button class="btn btn-link text-left text-dark collapsed" type="button" data-toggle="collapse"
+                        data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                        <span>Pengajuan Perbaikan Lab Bukalapak</span><br>
+                        <span>Jumat, 5 Februari 2020</span>
+                    </button>
+                </h2>
+                <div class="d-flex align-items-center">
+                    <span class="status-pill bg-danger text-white">Ditolak</span>
+                </div>
+            </div>
+            <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionHistory">
                 <div class="card-body">
                     Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
                     wolf
