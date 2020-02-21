27dc9e898a13bfd98325dc06c756509f8c6b29c9
I Putu Gede Wirasuta <13517015@std.stei.itb.ac.id>
2020-02-21 09:30:47 +0700

    Sprint 1 Product

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('master')
+
+@section('title', 'Welcome')
+
+@section('content')
+<h1>Welcome to PPL Monev</h1>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/persetujuan', function () {
+    return view('unitkerja.persetujuan');
+});
+Route::get('/pengajuan-jasa', function () {
+    return view('unitkerja.pengajuan');
+});
+
+Route::get('/riwayat-pengajuan', function () {
+    return view('unitkerja.riwayat');
+});
