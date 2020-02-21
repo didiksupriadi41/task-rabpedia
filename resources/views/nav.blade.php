f3cd06b162973b8a93c3844d4bb1a1ffc2d40efc
I Putu Gede Wirasuta <wirasutat@gmail.com>
2020-02-21 09:42:05 +0700

    hotfix for navbar links

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand font-weight-bold" href="#">RABPEDIA</a>
+    <a class="navbar-brand font-weight-bold" href="/">RABPEDIA</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -8,10 +8,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Katalog<span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="/pengajuan-jasa">Katalog<span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Riwayat</a>
+                <a class="nav-link" href="/riwayat-pengajuan">Riwayat</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/persetujuan">Persetujuan</a>
             </li>
         </ul>
         <ul class="navbar-nav">
