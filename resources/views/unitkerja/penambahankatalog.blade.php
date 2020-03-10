038af960a9ccf7605ce7a123bd4d3fbbd6e0cf39
Richard_Wellianto <13514051@std.stei.itb.ac.id>
2020-03-10 21:44:46 +0700

    add text/javascript in ajax js

--- resources/views/unitkerja/penambahankatalog.blade.php
+++ resources/views/unitkerja/penambahankatalog.blade.php
@@ -119,6 +119,6 @@
 
 @endsection
 @section('script-end')
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script type="text/javascript" src="{{ asset('js/penambahankatalog.js') }}"></script>
 @endsection
