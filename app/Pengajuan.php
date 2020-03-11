4322b52abbc6f720632d0b57c682d29ac1a66f14
Didik Supriadi <didiksupriadi41@gmail.com>
2020-03-11 19:19:11 +0700

    Update Pengajuan model definition

--- app/Pengajuan.php
+++ app/Pengajuan.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Pengajuan extends Model
 {
     protected $table = 'pengajuan';
+
+    protected $primaryKey = 'id_pengajuan';
+
+    protected $attributes = [
+        'status_pengajuan' => 'pending', // Proposal: status_pengajuan = {pending, accepted, rejected}
+    ];
 }
