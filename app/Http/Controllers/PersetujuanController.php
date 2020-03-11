019fb6b4faa9c77dc0544a3275dcf2ec75435463
Didik Supriadi <didiksupriadi41@gmail.com>
2020-03-11 19:26:47 +0700

    Create a controller for persetujuan

---
+++ app/Http/Controllers/PersetujuanController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Pengajuan;
+use Illuminate\Http\Request;
+
+class PersetujuanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      $pengajuan = Pengajuan::all();
+      return view('unitkerja.persetujuan')->with('listPengajuan', $pengajuan);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Pengajuan  $pengajuan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pengajuan $pengajuan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Pengajuan  $pengajuan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(int $id)
+    {
+        //
+      $pengajuan = Pengajuan::find($id);
+      return view('unitkerja.persetujuan')->with('pengajuan', $pengajuan);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Pengajuan  $pengajuan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pengajuan $pengajuan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Pengajuan  $pengajuan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pengajuan $pengajuan)
+    {
+        //
+    }
+}
