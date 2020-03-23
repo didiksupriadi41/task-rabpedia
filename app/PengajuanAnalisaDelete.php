300ab23fd0751a8c414bc78263d626d049873959
Richard_Wellianto <13514051@std.stei.itb.ac.id>
2020-03-23 09:45:03 +0700

    DB Pengajuan Analisa dan Model untuk DB Pengajuan

---
+++ app/PengajuanAnalisaDelete.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PengajuanAnalisaDelete extends Model
+{
+    protected $table = 'pengajuan_analisa_delete';
+}
---
+++ app/PengajuanBahanDelete.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PengajuanBahanDelete extends Model
+{
+    protected $table = 'pengajuan_bahan_delete';
+}
