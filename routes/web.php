3e942b983a5c7594cd1c8f48a04e7c95a40a1385
Josep Andre Ginting <42130509+josepandre99@users.noreply.github.com>
2020-03-26 17:56:26 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,18 @@
 |
 */
 
-// // Unprotected route
-// Route::get('/', function () {
-//     return view('welcome');
-// })->name('login');
+// Unprotected route
+Route::get('/', function () {
+    return view('welcome');
+})->name('login');
 
-// // Authentication routes
-// Route::get('/login-form', 'Auth\LoginController@showLoginForm');
-// Route::get('/sso-login', 'Auth\LoginController@ssoLogin');
-// Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+// Authentication routes
+Route::get('/login-form', 'Auth\LoginController@showLoginForm');
+Route::get('/sso-login', 'Auth\LoginController@ssoLogin');
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 
 // Routes protected by authentication middleware
-// Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/home', function () {
         return view('home');
     });
@@ -31,18 +31,38 @@
         return view('unitkerja.pengajuan');
     });
 
-    Route::get('/riwayat-pengajuan', 'StatusPengajuanController@detail_pengajuan');
+    Route::get('/riwayat-pengajuan', 'StatusPengajuanController@index');
 	Route::get('/lihat-katalog', 'LihatKatalogController@show_list_katalog');
-    Route::get('/penambahan-katalog', 'PenambahanKatalogJasaController@show_list_jasa');
-    
+	Route::get('/penambahan-katalog', 'PenambahanKatalogJasaController@show_list_jasa');   	
+
+    Route::get('/penambahan-ditlog', function(){
+        return view('unitkerja.penambahan.ditlog.home');
+    });
+    Route::get('/penambahan-bahan-ditlog', 'PenambahanBahanDitlogController@show_list_bahan');
+    Route::get('/penambahan-upah-ditlog', 'PenambahanUpahDitlogController@show_list_upah');
+    Route::get('/penambahan-material-ditlog', 'PenambahanMaterialDitlogController@show_list_material');
+    Route::get('/penambahan-pekerjaan-ditlog', 'PenambahanPekerjaanDitlogController@show_list_pekerjaan');
+
     Route::get('/riwayat-pengajuan/{id}', 'StatusPengajuanController@show');
 
-    // Route::get('/detail-pengajuan', function () {
-    //     return view('unitkerja.detail_pengajuan');
-    // });
-
     Route::resource('persetujuan', 'PersetujuanController');
-// });
+});
 
 Route::post('/deleterowanalisa', 'PenambahanKatalogJasaController@deleteAnalisa');
 Route::post('/insertrowanalisa', 'PenambahanKatalogJasaController@storeAnalisa');
+
+Route::post('/deleterowupahditlog', 'PenambahanUpahDitlogController@deleteUpah');
+Route::post('/insertrowupahditlog', 'PenambahanUpahDitlogController@storeUpah');
+Route::post('/updaterowupahditlog', 'PenambahanUpahDitlogController@updateUpah');
+Route::post('/deleterowbahanditlog', 'PenambahanBahanDitlogController@deleteBahan');
+Route::post('/insertrowbahanditlog', 'PenambahanBahanDitlogController@storeBahan');
+Route::post('/updaterowbahanditlog', 'PenambahanBahanDitlogController@updateBahan');
+Route::post('/deleterowmaterialditlog', 'PenambahanMaterialDitlogController@deleteMaterial');
+Route::post('/insertrowmaterialditlog', 'PenambahanMaterialDitlogController@storeMaterial');
+Route::post('/updaterowmaterialditlog', 'PenambahanMaterialDitlogController@updateMaterial');
+
+Route::post('/deleterowpekerjaanditlog', 'PenambahanPekerjaanDitlogController@deletePekerjaan');
+Route::post('/insertrowpekerjaanditlog', 'PenambahanPekerjaanDitlogController@storePekerjaan');
+Route::post('/deleterowanalisaditlog', 'PenambahanPekerjaanDitlogController@deleteAnalisa');
+Route::post('/insertrowanalisaditlog', 'PenambahanPekerjaanDitlogController@storeAnalisa');
+Route::post('/updaterowanalisaditlog', 'PenambahanPekerjaanDitlogController@updateAnalisa');
