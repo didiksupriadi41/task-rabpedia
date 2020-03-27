72fcbf1df27f3d9fff3fd807aade3222ff4174cf
I Putu Gede Wirasuta <wirasutat@gmail.com>
2020-03-27 10:30:12 +0700

    Add relationship on analisa to its master data

--- app/AnalisaUpah.php
+++ app/AnalisaUpah.php
@@ -8,4 +8,9 @@ class AnalisaUpah extends Model
 {
     protected $table = 'analisa_upah';
     protected $primaryKey = 'id_analisa_upah';
+
+    public function upah()
+    {
+        return $this->belongsTo('App\Upah', 'id_upah', 'id_upah');
+    }
 }
