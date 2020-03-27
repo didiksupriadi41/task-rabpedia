5baab7b88372caff09d0b5b4b25427ce95d4fe9d
I Putu Gede Wirasuta <13517015@std.stei.itb.ac.id>
2020-03-27 15:05:52 +0700

    Pengajuan analisa

--- app/AnalisaBahan.php
+++ app/AnalisaBahan.php
@@ -8,4 +8,9 @@ class AnalisaBahan extends Model
 {
     protected $table = 'analisa_bahan';
     protected $primaryKey = 'id_analisa_bahan';
+
+    public function bahan()
+    {
+        return $this->belongsTo('App\Bahan', 'id_bahan', 'id_bahan');
+    }
 }
